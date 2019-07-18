52373e78708db65549119a8fa1e8ba91bf1dd672
ozenc_taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2019-07-18 11:44:22 -0400

    Preprocessing and first-level analysis

--- code/preprocAndFirstLvl.py
+++ code/preprocAndFirstLvl.py
@@ -16,7 +16,11 @@ import os
 # Specify Folders not individual files
 # Don't put a slash at the end of your path but put one at the beginning (e.g. /home/Desktop/files)
 
-def AnalyzeCanine(path_to_mprage, path_to_epi, path_to_inVivo_atlas, path_to_design_folder, output_folder, path_to_ants_scripts):
+# Example:
+# preprocAndFirstLvl('/home/ozzy/Desktop/July8Canine/T1', '/home/ozzy/Desktop/July8Canine/EPI', '/home/ozzy/Desktop/July8Canine/Atlas', '/home/ozzy/Desktop/July8Canine/design', '/home/ozzy/Desktop/July8Canine/results', '/home/ozzy/bin/ants/bin') 
+
+
+def preprocAndFirstLvl(path_to_mprage, path_to_epi, path_to_inVivo_atlas, path_to_design_folder, output_folder, path_to_ants_scripts):
  
     ############################ PREPROCESSING ################################
     
@@ -47,37 +51,44 @@ def AnalyzeCanine(path_to_mprage, path_to_epi, path_to_inVivo_atlas, path_to_des
     warp_call = "%s/antsRegistrationSyN.sh -d 3 -f %s -m %s -o %s/dog -n 4"%(path_to_ants_scripts, template_path, averaged_mprage, warp_results_folder)
     os.system(warp_call)
 
-#    ################################# FEAT ####################################
-
+    ################################# FEAT ####################################
+    
     # Modify the fsf template for individual EPI and do the fMRI analysis
-    first_lvl_res = output_folder + "/first-level-results"
+    first_lvl_res = output_folder + "/first_level_results"
     if not os.path.exists(first_lvl_res):
         os.system("mkdir %s"%first_lvl_res)
     evonepath = path_to_design_folder + '/on'
     evtwopath = path_to_design_folder + '/off'
     for i in os.listdir(path_to_epi):
-        epi_output_folder = output_folder + "/first-level-results/%s"%i
+        if i[-3:] == ".gz":
+            epi_output_folder = output_folder + "/first_level_results/%s"%i[:-7]
+        else:
+            epi_output_folder = output_folder + "/first_level_results/%s"%i[:-4]
         epifullpath = path_to_epi + "/" + i
         ntime = os.popen("fslnvols %s"%epifullpath).read().rstrip()
         things_to_replace = {"SUBJECT_OUTPUT":epi_output_folder, "NTPTS":ntime, 
                              "STANDARD_IMAGE":template_path, "SUBJECT_EPI":epifullpath,
                              "SUBJECTEV1":evonepath, "SUBJECTEVI2":evtwopath}
-        with open("%s/design.fsf"%path_to_design_folder) as infile:
+        with open("%s/design_template.fsf"%path_to_design_folder, 'r') as infile:
             with open("%s/design.fsf"%path_to_design_folder,'w') as outfile:
                 for line in infile:
                     for src, target in things_to_replace.iteritems():
                         line = line.replace(src, target)
                     outfile.write(line)
-
-    os.system("feat %s"%path_to_design_folder+"/design.fsf")
-#    
-#    ########################### TRICK FSL #####################################
-#    
-#    ###################### GROUP LEVEL ANALYSIS ###############################
-#
-#    ######################### DEFORM Z-MAPS ###################################    
+        os.system("feat %s"%path_to_design_folder+"/design.fsf")
+        
+    ########################### TRICK FSL #####################################
+        
+    # Delete all mat files in each first-level, copy some identity matrices and
+    # mean functions so the registration and interpolation will not be effective. 
+    # Note: This code is still a part of the laop above but placed under a 
+    # different comment block since it's an important part of the analysis.
+        
+        os.system("rm -r %s.feat/reg/*mat"%epi_output_folder)
+        os.system("rm -r %s.feat/reg/standard.nii.gz"%epi_output_folder)
+        os.system("cp $FSLDIR/etc/flirtsch/ident.mat %s.feat/reg/example_func2standard.mat"%epi_output_folder)
+        os.system("cp %s.feat/mean_func.nii.gz %s.feat/reg/standard.nii.gz"%(epi_output_folder, epi_output_folder))    
     
-AnalyzeCanine('/home/ozzy/Desktop/test/T1', '/home/ozzy/Desktop/test/EPI', '/home/ozzy/Desktop/test/Atlas', '/home/ozzy/Desktop/test/design', '/home/ozzy/Desktop/test/results', '/home/ozzy/bin/ants/bin') 
     
     
     
