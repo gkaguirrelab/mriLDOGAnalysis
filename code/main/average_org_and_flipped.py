2327ad5f3917e6dde1a7812e34134bbdb15f0a43
Ozenc Taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2019-11-19 15:41:51 -0500

    Fixed an error

--- code/main/average_org_and_flipped.py
+++ code/main/average_org_and_flipped.py
@@ -8,7 +8,7 @@ def average_org_and_flipped(original_epi_path, flipped_epi_path, output_folder):
     if not os.path.exists(final_epi):
         os.system('mkdir %s' % final_epi)
     for i in os.listdir(original_epi_path):
-        avgcall = 'AverageImages 3 %s 0 %s %s' % (os.path.join(final_epi, 'final_' + i),
+        avgcall = 'AverageImages 4 %s 0 %s %s' % (os.path.join(final_epi, 'final_' + i),
                                                   os.path.join(original_epi_path, i),
                                                   os.path.join(flipped_epi_path, 'flipped_' + i))
         #avgcall = 'fslmaths %s -add %s -div 2 %s -odt float' % (os.path.join(original_epi_path, i),
