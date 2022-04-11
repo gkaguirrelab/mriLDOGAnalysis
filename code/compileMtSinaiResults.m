660a5aaea61118100c92ddc8b137b69012b15a5d
gkaguirre <gkaguirre@me.com>
2022-04-11 15:18:05 -0400

    Working photoFlicker pipeline

--- code/compileMtSinaiResults.m
+++ code/compileMtSinaiResults.m
@@ -21,19 +21,42 @@ sessionIDs = {...
     '606f4e3a23a4b1585ea16b77','61f8259d9c5e882ac6ff49da','618eaf46cc13a7db50926d34',...
     '5fbc010f007a24bdb2631e2a','5ff8a949a8232b19464b0926','5ffc9af3a2719d913b740e51',...
     '5fa5bbded20d5c2d63f359aa','5fa9611917ddac02d6e32d8d','5fb6c55208da2c5a2aa08e75',...
+    '60146008afe25f7a6015acd6','604a6351e9a059778e89359b',...
+    '609ac9b8267f663f4f609a9e','60a6ad7839ca5ed906a2557f',...
+    '611557d3c45699f4bee0f829','6127e127999da0d2e6c708f4',...
     };
+nSessions = length(sessionIDs);
 
 subjectNames = {...
     'N344','N347','N349',...
     '2350','2353','2356',...
     'Z663','Z665','Z666',...
+    'EM529','EM543',...
+    'EM529','EM543',...
+    'EM529','EM543',...
     };
+
+groupIdx = {[1 2 3],[4  6],[7 8 9],[10 11],[11 12],[13 14]};
+groupNames = {'WT','RCD1','XLPRA2','RHOT4R-pre1','RHOT4R-pre2','RHOT4R-post'};
+groupColors = {[0.5 0.5 0.5],[0 0 1],[1 0 0],[0 0.5 0],[0 0.5 0],[0 1 0]};
+nGroups = length(groupNames);
+jitterFactor = 0.1;
 
 % Some properties of the data and analyses
 stimulusDirections = {'L+S','L-S','LF'};
+nStimuli = length(stimulusDirections);
 ROIs = {'antV1','postV1','LGN'};
+nROIs = length(ROIs);
 theModelUsed = 'mtSinai';
 analysisIDs = [];
+
+yLimVals = {...
+    [-0.1 0.3],[-0.1,0.5],[-0.1 0.2],...
+    [-0.1 0.1],[-0.1 0.3],[-0.05 0.15],...
+    [-0.1,0.4],[-0.1 0.6],[-0.1 0.2],...
+    [-0.1,0.4],[-0.1 0.6],[-0.1 0.2],...
+    [-0.1,0.4],[-0.1 0.6],[-0.1 0.2],...
+    };
 
 % Obtain the list of mtSinai analysis IDs
 for ss = 1:length(sessionIDs)
@@ -57,7 +80,7 @@ end
 % Loop through the analaysis IDs, download the results file, store the
 % forwardModel output
 params=[];
-for ss=1:length(sessionIDs)
+for ss=1:nSessions
     for aa=1:length(ROIs)
         resultsFileName = [subjectNames{ss},'_',theModelUsed,'_results.mat'];
         saveLocation = fullfile(saveDir,resultsFileName);
@@ -80,13 +103,48 @@ for xx=1:length(ROIs)
     end
 end
 
+% Create a figure that summarizes the results
+figure
+for xx=1:nGroups
+    for yy=1:nStimuli
+        for zz=1:nROIs
+            plotIdx = (yy-1)*nStimuli+zz;
+            subplot(nStimuli,nROIs,plotIdx);
+            rowIdx = groupIdx{xx};
+            for ss=1:length(rowIdx)
+                thisMean = dataMeans(rowIdx(ss),(zz-1)*nROIs+yy);
+                thisSEM = dataSEMs(rowIdx(ss),(zz-1)*nROIs+yy);
+                if strcmp(ROIs(zz),'LGN')
+                    thisMean = -thisMean;
+                end
+                plot(xx+(ss-1)*jitterFactor,thisMean,'o','MarkerEdgeColor','k','MarkerFaceColor',groupColors{xx});
+                hold on
+                plot([xx+(ss-1)*jitterFactor xx+(ss-1)*jitterFactor],[thisMean-thisSEM,thisMean+thisSEM],'-','Color',groupColors{xx});
+            end
+            plot([0.5 3.5],[0 0],':k')
+            xticks([1:nGroups]);
+            xticklabels(groupNames);
+            title([ROIs{zz} '.' stimulusDirections{yy}])
+            xlim([0.5,nGroups+0.5]);
+            ylim(yLimVals{plotIdx});
+            ylabel('BOLD response [%d]')
+        end
+    end
+end
+
 % Create a table that summarizes the data
+dataStr=[];
+T=[];
 for xx=1:size(dataMeans,1)
     for yy=1:size(dataMeans,2)
         dataStr{xx,yy} = sprintf('%2.2f ± %2.2f',dataMeans(xx,yy),dataSEMs(xx,yy));
     end
 end
 T = array2table(string(dataStr));
-T.Properties.RowNames = subjectNames;
+% Row names combine subject name and group
+for ss=1:nSessions
+    rowName{ss} = [subjectNames{ss} '_' groupNames{cellfun(@(x) any(x==ss),groupIdx)}];
+end
+T.Properties.RowNames = rowName;
 T.Properties.VariableNames = varNames;
 
