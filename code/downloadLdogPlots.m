a5fdcfc0751976470f9e6a282ecb99bd76e5aae0
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2023-02-22 19:45:04 -0500

    Flattened cut plots

--- code/downloadLdogPlots.m
+++ code/downloadLdogPlots.m
@@ -1,3 +1,7 @@
+% If this is set, plot on flat surface
+plotFlat = 'True';
+setenv('LD_LIBRARY_PATH', ['/usr/lib/x86_64-linux-gnu:',getenv('LD_LIBRARY_PATH')]);
+
 % Ldog processing path
 saveFolder = '/home/ozzy/Desktop/plots';
 if ~isdir(saveFolder)
@@ -9,7 +13,7 @@ if ~isdir(tempDir)
 end
 pythonFuncSurf = '/home/ozzy/Documents/MATLAB/projects/mriLDOGAnalysis/code/plot_surface.py';
 ldogSurfaceCalc = '/home/ozzy/Desktop/invivo2exvivo';
-threshold = '0.1';
+threshold = '0.2';
 resampledTempPath = '/home/ozzy/Documents/MATLAB/projects/mriLDOGAnalysis/Atlas/invivo/2x2x2resampled_invivoTemplate.nii.gz';
 
 % Get Flywheel project 
@@ -48,18 +52,18 @@ for sub = 1:length(subjects)
                         region = 'V1';
                         hemi = 'Pseudo';
                         sessionType = 'photoFlicker';
-                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label))
+                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label), plotFlat)
                         region = 'LGN';
-                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label))                        
+                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label), plotFlat)                        
                     elseif contains(analyses{aa}.label, 'photoFlicker') && contains(analyses{aa}.label, 'noPseudo') && ~contains(analyses{aa}.label, 'LGN') && ~contains(analyses{aa}.label, 'allV1') && ~contains(analyses{aa}.label, 'wholeBrain')
                         matrix = fullfile(tempDir, 'mtSinai_results.mat');
                         analyses{aa}.downloadFile([subjects{sub}.label '_mtSinai_results.mat'], matrix);
                         region = 'V1';
                         hemi = 'noPseudo';
                         sessionType = 'photoFlicker';        
-                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label))
+                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label), plotFlat)
                         region = 'LGN';
-                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label))                        
+                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label), plotFlat)                        
                     % Max FLicker
                     elseif contains(analyses{aa}.label, 'maxFlicker') && contains(analyses{aa}.label, 'NoPseudo') && ~contains(analyses{aa}.label, 'LGN') && ~contains(analyses{aa}.label, 'allV1') && ~contains(analyses{aa}.label, 'wholeBrain')
                         matrix = fullfile(tempDir, 'maxFlicker_results.mat');
@@ -67,18 +71,18 @@ for sub = 1:length(subjects)
                         sessionType = 'maxFlicker';
                         hemi = 'noPseudoHemi';                      
                         region = 'V1';
-                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label))
+                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label), plotFlat)
                         region = 'LGN';
-                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label))
+                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label), plotFlat)
                     elseif contains(analyses{aa}.label, 'maxFlicker') && ~contains(analyses{aa}.label, 'NoPseudo') && ~contains(analyses{aa}.label, 'LGN') && ~contains(analyses{aa}.label, 'allV1') && ~contains(analyses{aa}.label, 'wholeBrain')
                         matrix = fullfile(tempDir, 'maxFlicker_results.mat');
                         analyses{aa}.downloadFile([subjects{sub}.label '_mtSinai_results.mat'], matrix);
                         sessionType = 'maxFlicker';
                         hemi = 'pseudoHemi';                      
                         region = 'V1';
-                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label))
+                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label), plotFlat)
                         region = 'LGN';
-                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label))
+                        process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, fullfile(saveFolder, subjects{sub}.label, sessions{ses}.label), plotFlat)
                     end
                     
                              
@@ -88,7 +92,7 @@ for sub = 1:length(subjects)
     end      
 end
 
-function process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, outputPath)
+function process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pythonFuncSurf, ldogSurfaceCalc, threshold, sessionType, outputPath, plotFlat)
     load(matrix, 'results')
     if strcmp(sessionType, 'photoFlicker')
         params = results.params;
@@ -104,9 +108,9 @@ function process_matrices(matrix, resampledTempPath, region, hemi, tempDir, pyth
     dat = MRIread(resampledTempPath);
     dat.vol = reshape(rightEye, [53 53 54]);
     MRIwrite(dat, fullfile(tempDir, 'rightEye.nii.gz'))
-    system(['python3 ' pythonFuncSurf ' ' [region '_' hemi '_' 'rightEye']  ' ' resampledTempPath ' ' fullfile(tempDir, 'rightEye.nii.gz') ' ' ldogSurfaceCalc ' ' threshold ' ' outputPath]);
+    system(['python3 ' pythonFuncSurf ' ' [region '_' hemi '_' 'rightEye']  ' ' resampledTempPath ' ' fullfile(tempDir, 'rightEye.nii.gz') ' ' ldogSurfaceCalc ' ' threshold ' ' outputPath ' ' plotFlat]);
     dat.vol = reshape(leftEye, [53 53 54]);
     MRIwrite(dat, fullfile(tempDir, 'leftEye.nii.gz'))
-    system(['python3 ' pythonFuncSurf ' ' [region '_' hemi '_' 'leftEye']  ' ' resampledTempPath ' ' fullfile(tempDir, 'leftEye.nii.gz') ' ' ldogSurfaceCalc ' ' threshold ' ' outputPath]);
+    system(['python3 ' pythonFuncSurf ' ' [region '_' hemi '_' 'leftEye']  ' ' resampledTempPath ' ' fullfile(tempDir, 'leftEye.nii.gz') ' ' ldogSurfaceCalc ' ' threshold ' ' outputPath ' ' plotFlat]);
     
 end
--- code/plot_surface.py
+++ code/plot_surface.py
@@ -4,7 +4,7 @@ import nilearn
 from nilearn import plotting
 import matplotlib.pyplot as plt
 
-def plot_surface(subject_id, invivoTemp, path_to_R2_map, ldog_surface_and_calculations_folder, threshold, output):
+def plot_surface(subject_id, invivoTemp, path_to_R2_map, ldog_surface_and_calculations_folder, threshold, output, flattenMap='False'):
     
     # This function makes surface plots from R2 stat maps.
     
@@ -16,6 +16,8 @@ def plot_surface(subject_id, invivoTemp, path_to_R2_map, ldog_surface_and_calcul
     # threshold: Threshold for the surface maps
     # output: Folder to save the images. Images are prenamed, so only point to the
     # folder you want to save images to.
+    # flattenMap: Plot images on flatten surfaces for ldog images. Need to be
+    # able to run freeview, and have access to ldog repo.
     
     threshold = float(threshold)
     print('Mapping to surface')
@@ -26,6 +28,8 @@ def plot_surface(subject_id, invivoTemp, path_to_R2_map, ldog_surface_and_calcul
     rh_inf = os.path.join(ldog_surface_and_calculations_folder, 'Woofsurfer', 'surf', 'rh.inflated')
     sulc_map_lh = os.path.join(ldog_surface_and_calculations_folder, 'Woofsurfer', 'surf', 'lh.sulc')
     sulc_map_rh = os.path.join(ldog_surface_and_calculations_folder, 'Woofsurfer', 'surf', 'rh.sulc')
+    lh_cut = os.path.join(ldog_surface_and_calculations_folder, 'Woofsurfer', 'surf', 'lh.flattened_cut')
+    rh_cut = os.path.join(ldog_surface_and_calculations_folder, 'Woofsurfer', 'surf', 'rh.flattened_cut')
     
     #Set path to calculations 
     warp = os.path.join(ldog_surface_and_calculations_folder,'exvivo_warp_files', 'toEx1Warp.nii.gz')
@@ -49,18 +53,27 @@ def plot_surface(subject_id, invivoTemp, path_to_R2_map, ldog_surface_and_calcul
         os.system('mri_vol2surf --mov %s --ref %s --reg %s --srcsubject Woofsurfer --hemi %s --o %s' % (interpolated_map, interpolated_map, register_dat, 'rh', os.path.join(surfaces_folder, 'right_hemi.mgz')))
       
         # Make the surface plots
-        loaded_lh_map = nilearn.surface.load_surf_data(os.path.join(surfaces_folder, 'left_hemi.mgz'))
-        loaded_rh_map = nilearn.surface.load_surf_data(os.path.join(surfaces_folder, 'right_hemi.mgz'))
-        loaded_inflated_left = nilearn.surface.load_surf_mesh(lh_inf)
-        loaded_inflated_right = nilearn.surface.load_surf_mesh(rh_inf)  
-        fig1 = plt.figure(figsize=[11,6])
-        fig2 = plt.figure(figsize=[11,6])
-        fig3 = plt.figure(figsize=[11,6])
-        fig4 = plt.figure(figsize=[11,6])
-        plotting.plot_surf_stat_map(loaded_inflated_left, loaded_lh_map, bg_map=sulc_map_lh,
-                                    threshold=threshold, view='medial', figure=fig1, output_file=os.path.join(output,'%s_left_medial.png' % subject_id), vmax=1) 
-        plotting.plot_surf_stat_map(loaded_inflated_right, loaded_rh_map, bg_map=sulc_map_rh,
-                                    threshold=threshold, view='lateral', figure=fig4, output_file=os.path.join(output,'%s_right_lateral.png' % subject_id), vmax=1)     
+        lh_map_path = os.path.join(surfaces_folder, 'left_hemi.mgz')
+        loaded_lh_map = nilearn.surface.load_surf_data(lh_map_path)
+        rh_map_path = os.path.join(surfaces_folder, 'right_hemi.mgz')
+        loaded_rh_map = nilearn.surface.load_surf_data(rh_map_path)
+        left_save_name = os.path.join(output,'%s_left_medial.png' % subject_id)
+        right_save_name = os.path.join(output,'%s_right_lateral.png' % subject_id)
+        
+        if flattenMap == 'True':
+            os.system('freeview --surface %s:patch=%s:curvature_method=binary:overlay=%s:overlay_threshold=%s,1 --cam Elevation 100 --screenshot %s' % (lh_inf,lh_cut,lh_map_path,threshold,left_save_name))
+            os.system('freeview --surface %s:patch=%s:curvature_method=binary:overlay=%s:overlay_threshold=%s,1 --cam Elevation 100 --screenshot %s' % (rh_inf,rh_cut,rh_map_path,threshold,right_save_name))            
+        else:
+            loaded_inflated_left = nilearn.surface.load_surf_mesh(lh_inf)
+            loaded_inflated_right = nilearn.surface.load_surf_mesh(rh_inf)  
+            fig1 = plt.figure(figsize=[11,6])
+            fig2 = plt.figure(figsize=[11,6])
+            fig3 = plt.figure(figsize=[11,6])
+            fig4 = plt.figure(figsize=[11,6])
+            plotting.plot_surf_stat_map(loaded_inflated_left, loaded_lh_map, bg_map=sulc_map_lh,
+                                        threshold=threshold, view='medial', figure=fig1, output_file=left_save_name, vmax=1) 
+            plotting.plot_surf_stat_map(loaded_inflated_right, loaded_rh_map, bg_map=sulc_map_rh,
+                                        threshold=threshold, view='lateral', figure=fig4, output_file=right_save_name, vmax=1)     
     elif 'LGN' in subject_id:
         plotting.plot_stat_map(path_to_R2_map, invivoTemp, threshold=threshold, title=subject_id, cut_coords=(7.8, -3.5, -13.5), output_file=os.path.join(output,'%s_LGN.png' % subject_id), vmax=1)
     
